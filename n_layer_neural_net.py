ac74b1f4cd988cebf1e17ba83cf421919341383e
chetan maddula <cm60@rice.edu>
2017-09-27 14:17:31 -0500

    backprop of layers and whole nn

--- n_layer_neural_net.py
+++ n_layer_neural_net.py
@@ -31,8 +31,10 @@ class DeepNeuralNetwork(NeuralNetwork):
         self.dW2 = (self.a1.T).dot(delta3)
         self.db2 = np.sum(delta3, axis=0, keepdims=True)
         delta1 = delta3.dot(self.W2.T) * self.diff_actFun(self.z1, self.actFun_type)
-        for layers in layer1:
-            delta1 = layers.backprop(X,y,delta1)
+        for i in range(self.num_layer,1,step = -1):
+            delta1 = layer1[i].backprop(X,y,delta1)
+        self.dW1 = np.dot(X.T,delta1)
+        self.db1 = np.sum(delta1, axis = 0)
 
 
     def calculate_loss(self, X, y):
@@ -40,20 +42,21 @@ class DeepNeuralNetwork(NeuralNetwork):
 
     def fit_model(self, X, y, epsilon=0.005, num_passes=20000, print_loss=True):
 
+
 class layer(NeuralNetwork):
     def __init__(self,id1,nn_input_dim, nn_hidden_dim, nn_output_dim, actFun_type='tanh', reg_lambda=0.01, seed=0):
         NeuralNetwork.__init__(self, nn_input_dim, nn_hidden_dim, nn_output_dim, actFun_type='tanh', reg_lambda=0.01,
                                seed=0)
         self.id = id1
         np.random.seed(seed)
-        self.W1 = np.random.randn(self.nn_hidden_dim, self.nn_hidden_dim) / np.sqrt(self.nn_input_dim)
-        self.b1 = np.zeros((1, self.nn_hidden_dim))
+        self.W = np.random.randn(self.nn_hidden_dim, self.nn_hidden_dim) / np.sqrt(self.nn_input_dim)
+        self.b = np.zeros((1, self.nn_hidden_dim))
 
 
     def feedforward(self, X, actFun):
-        self.z1 = X.dot(self.W1) + self.b1
-        self.a1 = self.actFun(self.z1, self.actFun_type)
-        return self.a1
+        self.z = X.dot(self.W) + self.b1
+        self.a = self.actFun(self.z, self.actFun_type)
+        return self.a
 
 
     def backprop(self, X, delta, y):
@@ -61,9 +64,11 @@ class layer(NeuralNetwork):
 
         self.dw = np.dot(X.T,delta)
         self.db = np.sum(self.delta,axis=0)
-        self.delta_next = self.delta.dot(self.W1.T)*self.diff_actFun(self.z1,self.actFun_type)
+        self.delta_next = self.delta.dot(self.W.T)*self.diff_actFun(self.z,self.actFun_type)
 
         return self.delta_next
+    def fit_model1(self,X,y):
+
 
 
 
