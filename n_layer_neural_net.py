20000bb34609ecebda9f2776b5c03aeb62ce3464
chetan maddula <cm60@rice.edu>
2017-09-26 21:49:12 -0500

    Finished 3 layer neural network and plots for various hidden layer sizes

--- n_layer_neural_net.py
+++ n_layer_neural_net.py
@@ -13,6 +13,16 @@ class DeepNeuralNetwork(NeuralNetwork):
     def feedforward(self, X, actFun):
 
     def backprop(self, X, y):
+        delta1 = num_examples = len(X)
+        delta3 = self.probs
+        delta3[range(num_examples), y] -= 1
+        dW2 = (self.a1.T).dot(delta3)
+        db2 = np.sum(delta3, axis=0, keepdims=True)
+        delta1 = delta3.dot(self.W2.T) * self.diff_actFun(self.z1, self.actFun_type)
+        for i in range(self.nn_layer):
+            layer1 = layer(self.nn_input_dim, self.nn_hidden_dim, self.nn_output_dim, self.actFun_type, self.reg_lambda)
+            delta1 = layer1.backprop(X,y,delta1)
+
 
     def calculate_loss(self, X, y):
 
@@ -32,11 +42,14 @@ class layer(NeuralNetwork):
         self.a1 = self.actFun(self.z1, self.actFun_type)
 
 
-    def backprop(self, X, y):
+    def backprop(self, X, delta, y):
         num_examples = len(X)
-        self.delta =
-        self.dw = np.dot(X.T,self.delta)
+
+        self.dw = np.dot(X.T,delta)
         self.db = np.sum(self.delta,axis=0)
+        self.delta_next = self.delta.dot(self.W1.T)*self.diff_actFun(self.z1,self.actFun_type)
+
+        return self.delta_next
 
 
 
