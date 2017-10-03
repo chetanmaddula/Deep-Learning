ad17ca0b618193deddf83c614d9ba9adfe69e1ba
chetan maddula <cm60@rice.edu>
2017-10-03 16:11:05 -0500

    sigmoid

--- dcn_mnist.py
+++ dcn_mnist.py
@@ -103,7 +103,7 @@ def main():
     # first convolutional layer
     W_conv1 = weight_variable([5, 5, 1, 32])
     b_conv1 = bias_variable([32])
-    h_conv1 = tf.tanh(conv2d(x_image, W_conv1) + b_conv1)
+    h_conv1 = tf.sigmoid(conv2d(x_image, W_conv1) + b_conv1)
     #h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
@@ -111,7 +111,7 @@ def main():
     W_conv2 = weight_variable([5,5,32,64])
     b_conv2 = bias_variable([64])
     #h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2) + b_conv2)
-    h_conv2 = tf.tanh(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_conv2 = tf.sigmoid(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
     # densely connected layer
@@ -119,7 +119,7 @@ def main():
     b_fc1 = bias_variable([1024])
     h_pool2_flat = tf.reshape(h_pool2,[-1, 7*7*64])
     #h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-    h_fc1 = tf.tanh(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    h_fc1 = tf.sigmoid(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     # dropout
     keep_prob = tf.placeholder(tf.float32)
