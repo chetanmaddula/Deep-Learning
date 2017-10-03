3295fffe0cd139e3b3afd4ec088937e695d59c9f
chetan maddula <cm60@rice.edu>
2017-10-02 22:21:49 -0500

    validation and test accuracy

--- dcn_mnist.py
+++ dcn_mnist.py
@@ -42,9 +42,9 @@ def bias_variable(shape):
 
 #
 
-##def leaky_relu (x, alp):
+def leaky_relu (x, alp):
 
-#    return tf.nn.relu(x) - alp * tf.nn.relu(-x)
+    return tf.nn.relu(x) - alp * tf.nn.relu(-x)
 
 #def tanh(x):
 
@@ -103,21 +103,23 @@ def main():
     # first convolutional layer
     W_conv1 = weight_variable([5, 5, 1, 32])
     b_conv1 = bias_variable([32])
-    #h_conv1 = leaky_relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_conv1 = leaky_relu(conv2d(x_image, W_conv1) + b_conv1)
+    #h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
     # second convolutional layer
     W_conv2 = weight_variable([5,5,32,64])
     b_conv2 = bias_variable([64])
-    h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2) + b_conv2)
+    #h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2) + b_conv2)
+    h_conv2 = leaky_relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
     # densely connected layer
     W_fc1 = weight_variable([7 * 7 * 64, 1024])
     b_fc1 = bias_variable([1024])
     h_pool2_flat = tf.reshape(h_pool2,[-1, 7*7*64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    #h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    h_fc1 = leaky_relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     # dropout
     keep_prob = tf.placeholder(tf.float32)
